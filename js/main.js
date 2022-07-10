f2b5cb36693ee7159671200db5bf2409bfdd57e3
Marcin Garski <marcin.garski@interia.eu>
2022-07-10 18:01:49 -0400

    Finish finish menu and profile section all media queries

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let typed = new Typed(".auto-type", {
-    strings: ["Front-end Developer", "Freelancer", "Programmer"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true,
-})
---
+++ js/typed.js
@@ -0,0 +1,13 @@
+let typed = new Typed(".auto-type", {
+    strings: ["Front-end Developer", "Freelancer", "Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+})
+
+let typedProfile = new Typed(".auto-type-profile", {
+    strings: ["Front-end Developer", "Freelancer", "Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+})
